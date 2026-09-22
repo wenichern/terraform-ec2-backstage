1f8b69382472933790eebe7a1498d3e67fe6216f
Allen Chen <wenichern@gmail.com>
2026-09-21 20:32:59 -0400

    Exclude unsupported AZ from subnet lookup

--- ami-poc-demo/terraform/ec2-demo/main.tf
+++ ami-poc-demo/terraform/ec2-demo/main.tf
@@ -42,6 +42,10 @@ data "aws_subnets" "default" {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
   }
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
 }
 
 resource "aws_instance" "app" {
